a9155f90f40fde50cd1d97aedd80f5c5d0db3f28
Manuel Borowski <manuel.borowski@campussintursula.be>
2023-01-11 19:15:41 +0100

    V0.11: added pip gevent-websocket

--- app/__init__.py
+++ app/__init__.py
@@ -32,11 +32,12 @@ flask_app.config.from_pyfile('config.py')
 # V0.8: added 'start fablab' link to home screen.  Removed view badge
 # V0.9: git bugfix: removed files from git
 # V0.10: git bugfix: removed files from git
+# V0.11: added pip gevent-websocket
 
 
 @flask_app.context_processor
 def inject_defaults():
-    return dict(version='@ 2022 MB. V0.10', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
+    return dict(version='@ 2022 MB. V0.11', title=flask_app.config['HTML_TITLE'], site_name=flask_app.config['SITE_NAME'])
 
 
 #  enable logging
